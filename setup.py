2fa757c79d5abb39ae52de544bca546006d107b6
Erik Härkönen <erik.harkonen@hotmail.com>
2023-01-19 17:00:45 +0900

    PyPi fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pathlib import Path
 
 import sys
 if 'develop' in sys.argv:
@@ -12,7 +13,7 @@ setup(name='pyviewer',
     description='Interactyive python viewers',
     author='Erik Härkönen',
     author_email='erik.harkonen@hotmail.com',
-    url='github.com/harskish/pyviewer',
+    url='https://github.com/harskish/pyviewer',
     packages=['pyviewer'], # name of importable thing
     setup_requires=['wheel'],
     install_requires=[
@@ -26,4 +27,8 @@ setup(name='pyviewer',
     package_data={
         '': ['*.ttf'] # embedded fonts
     },
+    long_description=Path('README.md').read_text()
+        .replace('docs/screenshot.jpg', 'https://github.com/harskish/pyviewer/raw/master/docs/screenshot.jpg'),
+    long_description_content_type="text/markdown",
+    license='CC BY-NC-SA 4.0',
 )
