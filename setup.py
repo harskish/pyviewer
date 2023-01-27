ab7a4f4cd71edae35a48279ac77d664022d0e9fa
Erik Härkönen <erik.harkonen@hotmail.com>
2023-01-28 00:51:28 +0900

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if 'develop' in sys.argv:
         dependencies correctly, please use 'pip install -e .' instead.")
 
 setup(name='pyviewer',
-    version='1.2.2',
+    version='1.3.0',
     description='Interactyive python viewers',
     author='Erik Härkönen',
     author_email='erik.harkonen@hotmail.com',
