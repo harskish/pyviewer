b457d774ef3e4b84d2407456b218312f5dc5982b
Erik Härkönen <erik.harkonen@hotmail.com>
2024-05-06 23:09:50 +0300

    Fix implot assert

--- pyviewer/__init__.py
+++ pyviewer/__init__.py
@@ -4,7 +4,7 @@ from . import gl_viewer
 
 # Check imgui version
 import imgui
-plot = getattr(imgui, 'plot')
+plot = getattr(imgui, 'plot', None)
 assert plot, \
     'Pyviewer requires a custom version of imgui that comes bundled with implot (github.com/harskish/pyimgui).\n' + \
     'Please reinstall pyviewer to get the correct version.'
